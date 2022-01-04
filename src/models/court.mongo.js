79fa114af76d819a916bfc96a0a01c5c1b7f23c4
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-04 18:14:30 +0530

    Define schema of a court

---
+++ src/models/court.mongo.js
@@ -0,0 +1,16 @@
+const mongoose = require("mongoose");
+
+const { Schema } = mongoose;
+
+const courtSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+});
+
+module.exports = mongoose.model("Courts", courtSchema);
