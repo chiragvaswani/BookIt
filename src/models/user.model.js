93896cbb2b68c7c9916d9e5ac907c98ed231ef7f
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-05 21:08:47 +0530

    Define function to create user

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,7 +1,27 @@
 const bcrypt = require("bcrypt");
 
 // For user sign up
-async function createUser() {}
+async function createUser(username, password) {
+  if (!((await findUser(username)) === null)) {
+    return res.status(406).json({
+      error: "Username already exists",
+    });
+  }
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const data = {
+      username,
+      password: hashedPassword,
+    };
+    await createUser(data);
+    return data.username;
+  } catch (err) {
+    console.error(err);
+    return -1;
+  }
+}
 
 // For user sign in
-async function signInUser() {}
+async function signInUser() {
+  username, password;
+}
