b1f8e4b3bee3f93522a764f5fce9d3b77d522f37
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-05 16:30:23 +0530

    Define a function that checks whether the slot is available and use it in createBooking

--- src/models/booking.model.js
+++ src/models/booking.model.js
@@ -1,35 +1,50 @@
 const booking = require("./booking.mongo");
 const courtsDatabase = require("./court.mongo");
 
+// Returns 1 if the booking was made. -1 otherwise
 async function createBooking(username, date, courtName, slot, cost) {
   court = await findCourt(courtName);
+  if (isBooked(date, courtName, slot)) {
+    console.error("The slot is unavailable.");
+    return -1;
+  }
   if (court) {
     const save = await saveBooking(
       username,
       date,
       courtName,
-      court.email,
+      "abc@xyz.com",
       slot,
       cost
     );
     if (save === -1) {
-      console.error("Error saving the booking");
+      console.error("Error saving the booking/");
       return;
+    } else {
+      console.log("The booking was made successfully.");
+      return 1;
     }
   } else {
     console.log("Entered court does not exist");
     return;
   }
-  console.log("Booking saved successfully.");
 }
 
 async function findCourt(courtName) {
   return await courtsDatabase.findOne({ courtName });
 }
 
+async function isBooked(date, courtName, slot) {
+  const res = await booking.find({
+    date: date,
+    courtName: courtName,
+    slot: slot,
+  });
+}
+
 async function saveBooking(username, date, courtName, email, slot, cost) {
   try {
-    await booking.findOneAndUpdate({
+    bookingDetails = await new booking({
       username,
       date,
       courtName,
@@ -37,6 +52,7 @@ async function saveBooking(username, date, courtName, email, slot, cost) {
       slot,
       cost,
     });
+    bookingDetails.save();
   } catch (err) {
     console.error(err);
     return -1;
