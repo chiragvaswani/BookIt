4e76e69294f9a3a172a79275bf0840d39b782905
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-05 22:54:13 +0530

    Add exports

--- index.js
+++ index.js
@@ -1,3 +1,24 @@
 const { mongoConnect, mongoDisconnect } = require("./src/services/mongo");
+const {
+  createBooking,
+  cancelBooking,
+  getUserBookings,
+} = require("./src/models/booking.model");
+const {
+  createCourt,
+  getCourtBookings,
+  findCourt,
+} = require("./src/models/court.model");
 
-module.exports = {};
+const { signUpUser, signInUser } = require("./src/models/user.model");
+
+module.exports = {
+  mongoConnect,
+  mongoDisconnect,
+  createBooking,
+  cancelBooking,
+  getUserBookings,
+  createCourt,
+  getCourtBookings,
+  findCourt,
+};
