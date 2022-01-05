3d00b260a615c654a17b04f4f61c08b86929e80f
chiragvaswani <chiragvaswani751@gmail.com>
2022-01-05 21:09:18 +0530

    Add flag to user schema

--- src/models/user.mongo.js
+++ src/models/user.mongo.js
@@ -11,6 +11,10 @@ const userSchema = new Schema({
     type: String,
     required: true,
   },
+  isOwner: {
+    type: Boolean,
+    required: true,
+  },
 });
 
 module.exports = mongoose.model("user", userSchema);
